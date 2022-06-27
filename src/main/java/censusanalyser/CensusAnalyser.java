054ed86ba1538133e51d2b3f455101e049e24ccc
priyankaBprabhu <priyankaprabhu96@gmail.com>
2022-06-27 16:39:52 +0530

    Added code to analyser to load the indian status census info from csv file

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -10,8 +10,23 @@ import java.nio.file.Paths;
 import java.util.Iterator;
 
 public class CensusAnalyser {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Indian States Census Analyser");
+    public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+            CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+            csvToBeanBuilder.withType(IndiaCensusCSV.class);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
+            Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();;
+            int namOfEateries = 0;
+            while (censusCSVIterator.hasNext()) {
+                namOfEateries++;
+                IndiaCensusCSV censusData = censusCSVIterator.next();
+            }
+            return namOfEateries;
+        } catch (IOException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        }
     }
-
 }
